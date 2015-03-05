25fd5c1cf0823c944ad33892b174b70de3cd920c
Toby Coleman <coleman-huntt@cardiff.ac.uk>
2015-03-05 12:25:47 +0000

    fixed bug

--- ccportal/ccportal/portlets/messenger/urls.py
+++ ccportal/ccportal/portlets/messenger/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # ex: /thread/
     url(r'^$', views.index, name='index'),
     # ex: /thread/5/
-    url(r'^(?P<thead_id>\d+)/$', views.thead, name='thread'),
+    # url(r'^(?P<thead_id>\d+)/$', views.thead, name='thread'),
     # ex: /thread/5/vote/
-    url(r'^(?P< thead_id >\d+)/delete/$', delete, name='delete'),
+    # url(r'^(?P< thead_id >\d+)/delete/$', delete, name='delete'),
 )
