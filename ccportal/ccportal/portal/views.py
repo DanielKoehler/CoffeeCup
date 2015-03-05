642f331cda62dc61e92e053b304697db62c8f0db
Daniel Arno George Koehler <koehlerda@cardiff.ac.uk>
2015-03-05 13:37:14 +0000

    Menu Update

--- ccportal/ccportal/portal/views.py
+++ ccportal/ccportal/portal/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def side_menu(func):
+    def wrapper(*args, **kwargs):
+        request = args[0]
+        menu_items = MenuItem.objects.all()
+        vars = {'menu_items': menu_items}
+        if kwargs:
+            page, template_var = func(*args, **kwargs)
+        else: page, template_var = func(*args)
+        vars.update( template_var)
+        return direct_to_template(request, page, vars)
+    return wrapper
