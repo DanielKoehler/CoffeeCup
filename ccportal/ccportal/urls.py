e01f7cd585236135288ce32ff9812d2527a86228
Toby Coleman <coleman-huntt@cardiff.ac.uk>
2015-03-05 12:29:59 +0000

    fixed import

--- ccportal/ccportal/urls.py
+++ ccportal/ccportal/urls.py
@@ -2,13 +2,16 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
+from ccportal.portlets import *
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'ccportal.portlets.dashboard.views.index', name='index'),
 
-    url(r'^messenger/', include(ccportal.portlets.messenger.urls)),
+    url(r'^messenger/', include(messenger.urls)),
 
 
 
