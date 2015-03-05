b14c72b4281e3e01f0b2e0ed8fd4653dfc335dcf
Daniel Arno George Koehler <koehlerda@cardiff.ac.uk>
2015-03-05 14:08:24 +0000

    Update templates

--- ccportal/ccportal/menus.py
+++ ccportal/ccportal/menus.py
@@ -2,39 +2,39 @@ from menu import Menu, MenuItem
 
 # Add two items to our main menu
 Menu.add_item("main", MenuItem("Tools",
-                               reverse("myapp.views.tools"),
+                               'link',
                                weight=10,
                                icon="tools"))
 
-Menu.add_item("main", MenuItem("Reports",
-                               reverse("myapp.views.reports"),
-                               weight=20,
-                               icon="report"))
+Menu.add_item("main", MenuItem("Meh",
+                               'link',
+                               weight=10,
+                               icon="tools"))
 
 
 # Define children for the my account menu
-myaccount_children = (
+# myaccount_children = (
 
     
-    MenuItem("Edit Profile",
-             # reverse("accounts.views.editprofile"),
-             weight=10,
-             icon="user"),
-    MenuItem("Admin",
-             # reverse("admin:index"),
-             weight=80,
-             separator=True,
-             check=lambda request: request.user.is_superuser),
-    MenuItem("Logout",
-             # reverse("accounts.views.logout"),
-             weight=90,
-             separator=True,
-             icon="user"),
+#     MenuItem("Edit Profile",
+#              # reverse("accounts.views.editprofile"),
+#              weight=10,
+#              icon="user"),
+#     MenuItem("Admin",
+#              # reverse("admin:index"),
+#              weight=80,
+#              separator=True,
+#              check=lambda request: request.user.is_superuser),
+#     MenuItem("Logout",
+#              # reverse("accounts.views.logout"),
+#              weight=90,
+#              separator=True,
+#              icon="user"),
 
-)
+# )
 
-# Add a My Account item to our user menu
-Menu.add_item("user", MenuItem("My Account",
-                               reverse("accounts.views.myaccount"),
-                               weight=10,
-                               children=myaccount_children))
+# # Add a My Account item to our user menu
+# Menu.add_item("user", MenuItem("My Account",
+#                                reverse("accounts.views.myaccount"),
+#                                weight=10,
+#                                children=myaccount_children))
